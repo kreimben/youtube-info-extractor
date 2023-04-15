82867196f0e8046540b047d653a16857b0958b23
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-15 21:13:24 +0900

    Refactoring

--- video/search_test.go
+++ video/search_test.go
@@ -6,9 +6,9 @@ import (
 
 func TestSearchVideoOne(t *testing.T) {
 	videoCh := make(chan *Video, 1)
-	go SearchVideoOne("위고 백지헌 직캠", videoCh)
+	go SearchOneVideoKeyword("위고 백지헌 직캠", videoCh)
 	if v, ok := <-videoCh; ok {
-		t.Log("No Playlist Video: ", v)
+		t.Log("No Playlist Video: ", v.Title)
 	} else if !ok {
 		t.Fatal("Error to get video")
 		return
@@ -18,9 +18,9 @@ func TestSearchVideoOne(t *testing.T) {
 func BenchmarkSearchVideoOne(b *testing.B) {
 	videoCh := make(chan *Video, 1)
 	for i := 0; i < b.N; i++ {
-		go SearchVideoOne("위고 백지헌 직캠", videoCh)
+		go SearchOneVideoKeyword("위고 백지헌 직캠", videoCh)
 		if v, ok := <-videoCh; ok {
-			b.Log("No Playlist Video: ", v)
+			b.Log("No Playlist Video: ", v.Title)
 		} else if !ok {
 			b.Fatal("Error to get video")
 			return
